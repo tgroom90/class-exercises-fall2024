344a72473f6c7a39d2630ec14d67c6c959caad3b
Travis Groom <tgroom@unca.edu>
2024-11-03 20:23:48 -0500

    Implemented RPS function and tests without framework

--- lab06/python_rps/run_tests_vanilla.py
+++ lab06/python_rps/run_tests_vanilla.py
@@ -27,10 +27,56 @@ def test_paper_beats_rock_flipped():
         'Paper beats rock (flipped)'  # output message
     )
 
+def test_rock_beats_scissors():
+    return assert_print(
+        rps('scissors', 'rock') == "Rock wins!",
+        'Rock beats scissors'
+    )
 
+def test_rock_beats_scissors_flipped():
+    return assert_print(
+        rps('rock', 'scissors') == 'Rock wins!',
+        'Rock beats scissors (flipped)'
+    )
+
+def test_scissors_beats_paper():
+    return assert_print(
+        rps('paper', 'scissors') == 'Scissors wins!',
+        'Scissors beats paper'
+    )
+
+def test_scissors_beats_paper_flipped():
+    return assert_print(
+        rps('scissors', 'paper') == "Scissors wins!",
+        'Scissors beats paper (flipped)'
+    )
+
+def test_rock_tie():
+    return assert_print(
+        rps('rock', 'rock') == 'Tie!',
+        'Rock and rock is a tie'
+    )
+def test_paper_tie():
+    return assert_print(
+        rps('paper', 'paper') == 'Tie!',
+        'Paper and paper is a tie'
+    )
+
+def test_scissors_tie():
+    return assert_print(
+        rps('scissors', 'scissors') == 'Tie!',
+        'Scissors and scissors is a tie'
+    )
 # don't forget to add any new tests to the list of tests to be run:
 run_all_tests([
     test_hello_world,
     test_paper_beats_rock,
-    test_paper_beats_rock_flipped
+    test_paper_beats_rock_flipped,
+    test_rock_beats_scissors,
+    test_rock_beats_scissors_flipped,
+    test_scissors_beats_paper,
+    test_scissors_beats_paper_flipped,
+    test_rock_tie,
+    test_paper_tie,
+    test_scissors_tie
 ])
--- lab06/python_rps/your_task.py
+++ lab06/python_rps/your_task.py
@@ -4,7 +4,17 @@ def hello_world():
 
 def rps(hand1, hand2):
     # finish this code:
-    if hand1 == "rock" and hand2 == "paper":
+    if (hand1 == "rock" and hand2 == "paper") or (hand1 == "paper" and hand2 == "rock"):
         return "Paper wins!"
+    elif (hand1 == "rock" and hand2 == "scissors") or (hand1 == "scissors" and hand2 == "rock"):
+        return "Rock wins!"
+    elif (hand1 == "paper" and hand2 == "scissors") or (hand1 == "scissors" and hand2 == "paper"):
+        return "Scissors wins!"
+    elif hand1 == "paper" and hand2 == "paper":
+        return "Tie!"
+    elif hand1 == "rock" and hand2 == "rock":
+        return "Tie!"
+    elif hand1 == "scissors" and hand2 == "scissors":
+        return "Tie!"
     else:
         return "Invalid"
